654fbe03cd3b7eae4c6fe4b6ef3b0c845a830b44
ayz1070 <ayz1070@gmail.com>
2025-02-06 23:29:50 +0900

    [session4] 공통 인터페이스 기능

--- src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -5,6 +5,9 @@ import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Repository;
 import study.datajpa.entity.Member;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public class MemberJpaRepository {
 
@@ -16,7 +19,29 @@ public class MemberJpaRepository {
         return member;
     }
 
+    public void delete(Member member) {
+        em.remove(member);
+    }
+
+    // JPQL 방식
+    // 객체를 중심으로
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class).getResultList();
+    }
+
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    public long count(){
+        return em.createQuery("select count(m) from Member m", Long.class).getSingleResult();
+    }
+
     public Member find(Long id) {
         return em.find(Member.class, id);
     }
+
+    // 업데이트는 필요없다.
+    // 객체를 변경하면 그냥 수정되는 것을 인지해서 알아서 update 쿼리를 보낸다.
 }
