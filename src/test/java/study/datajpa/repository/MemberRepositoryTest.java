4fdae196dfdf90207848b7a134f740b077dfd590
ayz1070 <ayz1070@gmail.com>
2025-02-10 22:54:33 +0900

    [session5] 쿼리 메소드 기능

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -10,6 +10,7 @@ import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 import study.datajpa.entity.Team;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -118,6 +119,19 @@ public class MemberRepositoryTest {
         List<MemberDto> memberDtos = memberRepository.findMemberDto();
         for(MemberDto memberDto : memberDtos){
             System.out.println("dto = " + memberDto);
+        }
+    }
+
+    @Test
+    public void findByuNames() {
+        Member member1 = new Member("AAA",10);
+        Member member2 = new Member("BBB",20);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        List<Member> result = memberRepository.findByNames(Arrays.asList("AAA","BBB"));
+        for(Member member : result){
+            System.out.println("member = " + member);
         }
     }
 }
