e6ceeb4a491dec91c6178f2a274989c4e77c249e
ayz1070 <ayz1070@gmail.com>
2025-02-11 22:46:29 +0900

    [session5] 쿼리 메소드 기능

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -209,5 +209,63 @@ public class MemberRepositoryTest {
         assertThat(resultCount).isEqualTo(2);
     }
 
+    @Test
+    public void findMemberLazy(){
+        // given
+        // member1 -> teamA
+        // member2 -> teamB
+
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        teamRepository.save(teamA);
+        teamRepository.save(teamB);
+
+        Member member1 = new Member("member1",10,teamA);
+        Member member2 = new Member("member2",20,teamB);
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        em.flush();
+        em.clear();
+
+        //when
+        List<Member> members = memberRepository.findEntityGraphByUsername("member1");
+
+        for(Member member : members){
+            System.out.println("member = " + member.getUsername());
+            System.out.println("member = " + member.getTeam().getName());
+        }
+
+    }
+
+    @Test
+    public void queryHint(){
+        //givne
+        Member member1 = new Member("member1",10);
+        memberRepository.save(member1);
+        em.flush();
+        em.clear();
+
+        //when
+        Member findMember = memberRepository.findReadOnlyByUsername("member1");
+        findMember.setUsername("member2");
+
+        em.flush();
+    }
+
+    @Test
+    public void lock(){
+        //givne
+        Member member1 = new Member("member1",10);
+        memberRepository.save(member1);
+        em.flush();
+        em.clear();
+
+        //when
+        List<Member> result = memberRepository.findLockedByUsername("member1");
+    }
+
+
 
 }
